7cb07f2a32a45c0181773731398d66063ccd3d58
shashankNT <shashank.k@navtech.io>
2025-05-06 20:35:27 +0530

    Install react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import Footer from "./components/Footer";
+import NavBar from "./components/NavBar";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <BrowserRouter>
+      </BrowserRouter>
+    </>
   )
 }
