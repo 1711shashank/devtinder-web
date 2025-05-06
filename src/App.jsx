9f26f05c2c40ac3b538c06799ca4f2524cd022ca
shashankNT <shashank.k@navtech.io>
2025-05-06 20:39:13 +0530

    install react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Footer from "./components/Footer";
 import NavBar from "./components/NavBar";
 
 export default function App() {
   return (
     <>
-      <BrowserRouter>
+      <BrowserRouter basename='/'>
+        <Routes>
+          <Route path="/" element={<div>Base Page</div>} />
+          <Route path="/login" element={<div>login Page</div>} />
+          <Route path="/test" element={<div>test Page</div>} />
+        </Routes>
       </BrowserRouter>
     </>
   )
