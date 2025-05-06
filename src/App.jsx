04bbe246be74ca323ac23f231774d5a337faef04
shashankNT <shashank.k@navtech.io>
2025-05-06 23:37:36 +0530

    login page + setup redux store

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Body from "./components/Body";
+import Login from "./components/Login";
+import appStore from "./utils/appStore";
+import { Provider } from "react-redux";
 
 export default function App() {
   return (
     <>
-      <BrowserRouter basename='/'>
-        <Routes>
+      <Provider store={appStore}>
+        <BrowserRouter basename='/'>
+          <Routes>
 
-          <Route path="/" element={<Body />}>
-            <Route path="/login" element={<div>login Page</div>} />
-            <Route path="/test" element={<div>test Page</div>} />
-          </Route>
+            <Route path="/" element={<Body />}>
+              <Route path="/login" element={<Login />} />
+              <Route path="/test" element={<div>test Page</div>} />
+            </Route>
 
-        </Routes>
-      </BrowserRouter>
+          </Routes>
+        </BrowserRouter>
+      </Provider>
     </>
   )
 }
