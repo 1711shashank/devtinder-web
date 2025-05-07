3cabc8814dc3d5299db59b55f78f211f0cf595be
shashankNT <shashank.k@navtech.io>
2025-05-07 17:33:35 +0530

    fetch userData when reload

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import axios from 'axios'
 import NavBar from './NavBar'
-import { Outlet } from 'react-router-dom'
 import Footer from './Footer'
+import React, { useEffect } from 'react'
+import { addUser } from '../utils/userSlice'
+import { BASE_URL } from '../utils/constants'
+import { Outlet, useNavigate } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
 
 const Body = () => {
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const userData = useSelector((store) => store.user.userData);
+
+    const fetchUser = async () => {
+        if (userData) return;
+        try {
+            const res = await axios.get(BASE_URL + "/profile/view", {
+                withCredentials: true,
+            });
+            dispatch(addUser(res.data));
+        } catch (err) {
+            if (err.status === 401) {
+                navigate("/login");
+            }
+            console.error(err);
+        }
+    };
+
+    useEffect(() => {
+        fetchUser();
+    }, []);
+
     return (
         <div>
             <NavBar />
